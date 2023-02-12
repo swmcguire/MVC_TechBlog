f1691424a231029fd283a34684ce7cbba97e5add
swmcguire <swmcguire1@comcast.net>
2023-02-12 15:38:53 -0600

    updated models and controllers

--- models/Blog.js
+++ models/Blog.js
@@ -18,11 +18,11 @@ Blog.init(
         message: {
           type: DataTypes.STRING,
         },
-        date_created: {
+        /*date_created: {
           type: DataTypes.DATE,
           allowNull: false,
           defaultValue: DataTypes.NOW,
-        },
+        },*/
         user_id: {
           type: DataTypes.INTEGER,
           references: {
@@ -36,7 +36,7 @@ Blog.init(
         timestamps: false,
         freezeTableName: true,
         underscored: true,
-        modelName: 'project',
+        modelName: 'blog',
       }
 );
 
